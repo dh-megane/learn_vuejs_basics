7749e03ed62838e22a77743eeb8b168d3a98cf54
Yasuda Toshiyuki <toshiyuki.yasuda@icare.jpn.com>
2018-08-31 15:59:39 +0900

    勉強会の中で作成したファイルを追加

---
+++ hands_on1/main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    list: [
+      {id: 1, name: 'Xがた', hp: 1000 },
+      {id: 2, name: 'Xむら', hp: 900 },
+      {id: 3, name: 'のX', hp: 800 },
+      {id: 4, name: '安X', hp: 7000 }
+    ]
+  }
+})
